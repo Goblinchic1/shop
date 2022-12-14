defa1625773719331e5d4cb6bf4f81d6089f512c
Goblinchic <maximf98@mail.ru>
2022-12-14 22:20:39 +0500

    add TelegramBotApiException.php

---
+++ app/Services/Telegram/Exceptions/TelegramBotApiException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Telegram\Exceptions;
+
+use Exception;
+use Illuminate\Http\Request;
+
+final class TelegramBotApiException extends Exception
+{
+
+}
